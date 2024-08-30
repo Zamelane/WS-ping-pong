3d7507eaaf64c812838b37a83edec407f7d61314
zamelane <zamelane@vk.com>
2024-08-30 18:43:04 +0700

    Indent Using Spaces

--- src/utils/argsReader.ts
+++ src/utils/argsReader.ts
@@ -2,31 +2,31 @@ import type { IArg, ISearchArg } from "../interfaces"
 
 
 export default (args: ISearchArg[]) => {
-    const argsValues: IArg = {}
-    const parg = process.argv
+	const argsValues: IArg = {}
+	const parg = process.argv
 
-    for (let i = 0; i < parg.length; i++) {
+	for (let i = 0; i < parg.length; i++) {
 
-        for (let j = 0; j < args.length; j++) {
-            const currArg = args[j]
-            const names = currArg.otherNames.concat(currArg.defaultName)
+		for (let j = 0; j < args.length; j++) {
+			const currArg = args[j]
+			const names = currArg.otherNames.concat(currArg.defaultName)
 
-            for (let t = 0; t < names.length; t++){
-                const checkParg = parg[i].toLowerCase()
-                const findArg = `${names[t].length == 1 ? '-' : '--'}${names[t]}`.toLowerCase()
-                
-                if (checkParg === findArg) {
-                    argsValues[currArg.defaultName] = (typeof currArg.defaultValue === 'boolean' ? true : parg[++i])
-                    break
-                }
-            }
+			for (let t = 0; t < names.length; t++) {
+				const checkParg = parg[i].toLowerCase()
+				const findArg = `${names[t].length == 1 ? '-' : '--'}${names[t]}`.toLowerCase()
 
-            let checkFound = argsValues[currArg.defaultName]
+				if (checkParg === findArg) {
+					argsValues[currArg.defaultName] = (typeof currArg.defaultValue === 'boolean' ? true : parg[++i])
+					break
+				}
+			}
 
-            argsValues[currArg.defaultName] = checkFound ?? (typeof currArg.defaultValue === 'boolean' ? false : currArg.defaultValue)
-        }
+			let checkFound = argsValues[currArg.defaultName]
 
-    }
+			argsValues[currArg.defaultName] = checkFound ?? (typeof currArg.defaultValue === 'boolean' ? false : currArg.defaultValue)
+		}
 
-    return argsValues
+	}
+
+	return argsValues
 }
